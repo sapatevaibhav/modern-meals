cf35b2b84f569b7a68deb4cee1d1d35a62c7bcf1
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-29 09:54:39 +0530

    added CateringBanner to homescreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import CateringBanner from './components/CateringBanner'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Navbar/>
+     <CateringBanner/>
     </>
   )
 }
---
+++ src/components/CateringBanner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CateringBanner.css";
+
+const CateringBanner = () => {
+  return (
+    <div className="banner">
+    </div>
+  );
+};
+
+export default CateringBanner;
