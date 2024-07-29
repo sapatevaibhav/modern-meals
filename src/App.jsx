4478267a802b9640fd6ccbd1de9228bb9a40bb38
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-29 23:46:53 +0530

    added about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import "./App.css";
+import AboutUs from "./components/AboutUs";
 import CateringBanner from "./components/CateringBanner";
 import Carousel from "./components/Description";
 import Gallery from "./components/HomeGallery";
 import MagicalExperience from "./components/MagicalExperience";
 import Navbar from "./components/Navbar";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
       <Carousel />
       <MagicalExperience />
       <Gallery />
+      <Testimonials />
+      <AboutUs />
     </>
   );
 }
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <img src="/logo.png" alt="Modern Meals Logo" className="logo" />
+      <div className="content">
+        <div className="about">
+          <h3>About Us</h3>
+          <p>
+            In the context of this current social scenario in India, it is not
+            surprising that the demand for top quality function organizers is
+            witnessing an ever increasing trend at present. More and more
+            families are opting for professionals to take care of their special
+            functions. And when it comes down to choosing the most
+            professionally managed wedding organizers, there are not many who
+            can stand in the same league as Modern Caterers.
+          </p>
+        </div>
+        <div className="links-contacts">
+          <div className="quick-links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <Link to="/about">About us</Link>
+              </li>
+              <li>
+                <Link to="/">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="/">Services</Link>
+              </li>
+              <li>
+                <Link to="/">Gallery</Link>
+              </li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="contacts">
+            <h3>Contacts</h3>
+            <p>address: B-151 North Ex-Mall, Sector-9, Rohini, Pune-411010</p>
+            <p>phone: +91 - 8800209900, 8800479900</p>
+            <p>email: info@modernmeals.com</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <footer>
+        © 2023-24 Modern Meals Catering & Events. All rights reserved.
+      </footer>
+    </div>
+  );
+};
+
+export default AboutUs;
