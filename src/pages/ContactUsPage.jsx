c37b0558ec88cf8e96e9fcb04d02fa044c7ff037
sapatevaibhav <sapatevaibhav@duck.com>
2024-08-03 15:15:03 +0530

    removed google maps api dependency

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,75 +1,24 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import "./ContactUsPage.css";
 import ContactComponent from "../components/ContactComponent";
 
-const containerStyle = {
-  height: "500px",
-  width: "100%",
-};
-
-const center = {
-  lat: 18.516726,
-  lng: 73.856255,
-};
-
-function MapComponent() {
-  const [map, setMap] = useState(null);
-
-  const onLoad = useCallback((mapInstance) => {
-    setMap(mapInstance);
-  }, []);
-
-  useEffect(() => {
-    if (map && window.google && window.google.maps) {
-      const marker = new window.google.maps.Marker({
-        position: center,
-        map,
-      });
-
-      const infoWindow = new window.google.maps.InfoWindow({
-        content: "<div>A pretty CSS3 popup. <br /> Easily customizable.</div>",
-      });
-
-      marker.addListener("click", () => {
-        infoWindow.open(map, marker);
-      });
-    }
-  }, [map]);
-
-  return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={15}
-      onLoad={onLoad}
-    />
-  );
-}
-
 export default function ContactUsPage() {
-  const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    id: "google-map-script",
-  });
-
-  if (loadError) {
-    return <div>Error loading maps</div>;
-  }
-
   return (
     <div className="contact-container">
       <div className="contact-header text-center">
-        <h1 className="custom-headings" style={{marginTop:"20px"}}>Contact Us</h1>
+        <h1 className="custom-headings contact-heading">Contact Us</h1>
         <p className="contact-subtitle">Best of Dining Experience</p>
       </div>
       <ContactComponent />
-      <div className="container google-map-container my-5 " style={{margin:"auto",minWidth:"1165px"}}>
-        {isLoaded ? (
-          <MapComponent />
-        ) : (
-          <div style={{ height: "500px" }}>Loading map...</div>
-        )}
+      <div className="container google-map-container my-5">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.3889993449607!2d73.86553929972337!3d18.511315346720156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c041ba5058c7%3A0x9ab0649cda0bff2f!2sShaan%20Darshan%20Building%2C%20Sadashiv%20Govind%20Rasta%2C%20New%20Nana%20Peth%2C%20Bhawani%20Peth%2C%20Pune%2C%20Maharashtra%20411002!5e0!3m2!1sen!2sin!4v1722677146469!5m2!1sen!2sin"
+          width="1140"
+          height="600"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </div>
   );
